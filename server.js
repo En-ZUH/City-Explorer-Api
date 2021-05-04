7a8d17f2a3df76d8094eb8b05a8f760ddc81c155
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-05-04 03:30:16 +0300

    done

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const data = require('./Data/weather.json')
+const { json } = require('express')
+require('dotenv').config()
+let PORT = process.env.PORT
+const app = express()
+app.use(cors())
+app.get('/', function (req, res) {
+    res.send(data)
+})
+app.listen(PORT)
+
+
+
+
+
