6526ecae952ad9e7ea684756f9269ac5e3da8698
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-05-04 22:24:44 +0300

    done

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const weather = require('./asesst/data/weather.json')
+const { json } = require('express')
+require('dotenv').config()
+let PORT = process.env.PORT
+const app = express()
+app.use(cors())
+app.get('/', function (req, res) {
+    res.send(data)
+})
+app.get('/weather', function (req, res) {
+    const arrOfData = weather.data.map(data => new Weather(data));
+    res.send(arrOfData);
+})
+class Weather {
+    constructor(data) {
+        this.date = data.valid_date;
+        this.description = data.weather.description;
+    }
+}
+app.listen(PORT)
