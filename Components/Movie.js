0e778d8b802cc9392f87b483893f9b41b90bd3be
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-05-11 01:42:43 +0300

    done

--- Components/Movie.js
+++ Components/Movie.js
@@ -1,6 +1,31 @@
-const superagent = require('superagent');
 require('dotenv').config();
+let MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+const superagent = require('superagent');
 
 
+const getMovie = (req, res) => {
+    try {
+        //console.log(req.query);
+        const movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${req.query.query}`;
 
-module.exports = handleWather;
+        superagent.get(movieURL).then(movieData => {
+            const arrOfData = movieData.body.results.map(data => new Movie(data));
+            res.send(arrOfData);
+
+        })
+    }
+    catch (error) {
+        console.log('error');
+    };
+
+};
+
+class Movie {
+    constructor(data) {
+        this.title = data.original_title;
+        this.overview = data.overview;
+        this.movieImage = data.poster_path;
+    }
+}
+
+module.exports = getMovie;
--- server.js
+++ server.js
@@ -1,22 +1,14 @@
-const express = require('express')
-const superagent = require('superagent');
-
-const cors = require('cors')
-
-const weather = require('./Data/weather.json') //import weather file
-const { json } = require('express')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const { json } = require('express');
+require('dotenv').config();
+const showWeather = require('./Components/Weather');
+const app = express();
+app.use(cors());
+const showMovie = require('./Components/Movie');
 
 
 let PORT = process.env.PORT
-let WEATHER_API_KEY = process.env.WEATHER_API_KEY
-let MOVIE_API_KEY = process.env.MOVIE_API_KEY
-
-
-
-const app = express()
-
-app.use(cors())
 
 
 app.get('/', function (req, res) {
@@ -24,75 +16,11 @@ app.get('/', function (req, res) {
   res.send('check')
 })
 
-// STATIC
-// creating a new end-point  (requsedt data from own API-staticlly)
+// console.log(showWeather.name);
+// app.get('/weather', showWeather.getWeather); in case of exporting obj, contains more than a fun
 
-//app.get('/weather', function (req, res) {
-//const arrOfData = weather.data.map(data => new Weather(data));  // mapping through the data to get an array for each day
-//res.send(arrOfData);     //send the array to the cleint who requested the data from weather end-point
-// })
+app.get('/weather', showWeather);
 
-
-// DYNAMIC ..to get the data from weatherbit
-app.get('/weather', function (req, res) {
-  try {
-    //console.log(req.query); //to accses the request query string(search queries)(here is lon and lat)
-    // res.send('weatherbit');
-    const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${req.query.lat}&lon=${req.query.lon}`;
-
-
-    superagent.get(weatherUrl).then(bitData => {
-      const arrOfData = bitData.body.data.map(data => new Weather(data));
-      res.send(arrOfData);
-      //.body beacuse we will not work on the div.. we need to work on the body to get the actual data
-    }).catch(console.error());
-  }
-  catch (error) {
-    const arrOfData = weather.data.map(data => new Weather(data));  //to get the data statically, in case of error
-    res.send(arrOfData);
-
-  };
-
-});
-
-class Weather {
-  constructor(data) {
-    this.date = data.valid_date;
-    this.description = data.weather.description;
-  }
-}
-
-
-
-
-
-
-
-
-app.get('/movie', function (req, res) {
-  try {
-    //console.log(req.query);
-    const movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${req.query.query}`;
-
-    superagent.get(movieURL).then(movieData => {
-      const arrOfData = movieData.body.results.map(data => new Movie(data));
-      res.send(arrOfData);
-
-    })
-  }
-  catch (error) {
-    console.log('error');
-  };
-
-});
-
-class Movie {
-  constructor(data) {
-    this.title = data.original_title;
-    this.overview = data.overview;
-    this.movieImage = data.poster_path;
-  }
-}
-
+app.get('/movie', showMovie);
 
 app.listen(PORT, () => console.log(`app is listening on ${PORT}`))
